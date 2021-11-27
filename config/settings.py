e9c3353126ac10f0449bd5fceee8513935bf37fc
mehediJoy <mhjoy1998@gmail.com>
2021-11-27 18:01:05 +0600

    Added Crispy and Bootstrap

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'crispy_forms',
+    
     'accounts',
     'pages',
 ]
@@ -126,3 +128,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
