d8450d59db4cff54659d54cb23af4e8e4645b8bb
mehediJoy <mhjoy1998@gmail.com>
2021-12-11 00:31:57 +0600

    Added Gmail SMTP and Production Configs

--- config/settings.py
+++ config/settings.py
@@ -134,4 +134,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'joy.devs0632@gmail.com'
+EMAIL_HOST_PASSWORD = 'gktietpnmfjowzha' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'NewsApp Support Team'
